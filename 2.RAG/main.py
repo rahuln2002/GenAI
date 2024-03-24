b4d33f7e0112888fd91eea0998990986a731aa59
Rahul Nihalani <rahulnihalani007@gmail.com>
2024-03-24 16:57:06 +0530

    Add files via upload

--- 2.RAG/main.py
+++ 2.RAG/main.py
@@ -1,29 +1,11 @@
 import os
 import streamlit as st
-from langchain.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_openai import OpenAIEmbeddings
-from langchain.vectorstores import Chroma
-from langchain_openai import ChatOpenAI
-from langchain.chains import RetrievalQA
+from embedchain import App
 
 os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
 
-
-loader = PyPDFLoader("./Harry_Potter_all_books_preprocessed.txt")
-data = loader.load()
-
-text_splitter = RecursiveCharacterTextSplitter(chunk_size = 300, chunk_overlap = 50)
-all_splits = text_splitter.split_documents(data)
-
-vectorstore = Chroma.from_documents(documents=all_splits, embedding=OpenAIEmbeddings())
-
-llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
-
-qa_chain = RetrievalQA.from_chain_type(
-    llm,
-    retriever=vectorstore.as_retriever()
-)
+bot = App()
+bot.add("./Harry_Potter_all_books_preprocessed.txt")
 
 st.title("Harry Potter 🔮")
 st.subheader("Ask about Harry Potter Series")
@@ -31,5 +13,5 @@ st.subheader("Ask about Harry Potter Series")
 question = st.text_input("Ask Question!")
 
 if st.button("Generate"):
-    result = qa_chain({"query": question})
-    st.write(result['result'])
+    result = bot.query(question)
+    st.write(result)
